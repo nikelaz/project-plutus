fd2792fe54dbe7b0365a5d0ad16f321a46d706ce
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2024-01-31 18:51:27 +0200

    create application class and start working on the user model

---
+++ src/application.js
@@ -0,0 +1,7 @@
+class Application {
+  main() {
+
+  }
+}
+
+module.exports = Application;
---
+++ src/models/user.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now, 
+  },
+});
+
+const User = mongoose.model('User', userSchema);
